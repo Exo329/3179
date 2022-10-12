170d55c690657b344e8b8bd66e25a55a76202670
sam_karopoulos <sjkar3@student.monash.edu.au>
2022-10-12 15:37:03 +1100

    Data Viz 2. Created basic line chart with accompanying js and html to create a basic webpage.

---
+++ DataViz2/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "global_land_temperature_over_time_chart.vg.json";
+vegaEmbed("#global_land_temperature_over_time_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
