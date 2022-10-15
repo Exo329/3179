8d57cd743c14882c1dab299358f17fb1f36ee93a
sam_karopoulos <sjkar3@student.monash.edu.au>
2022-10-16 03:10:08 +1100

    Data Viz 2. Renamed global_land_temperature_over_time.vg.json to global_average_surface_temperature_over_time.vg.json as this now reflects surface temperature rather than land temperature. Finalised this chart. Updated .js and .html appropriately but html appears bugged when I test it.

--- DataViz2/vega_lite_vis.js
+++ DataViz2/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "global_land_temperature_over_time_chart.vg.json";
-vegaEmbed("#global_land_temperature_over_time_chart", vg_1).then(function(result) {
+var vg_1 = "global_average_surface_temperature_over_time.vg.json";
+vegaEmbed("#global_average_surface_temperature_over_time", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
