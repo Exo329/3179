184918b610d2217f18846303cb1fa7b37ec5152f
sam_karopoulos <sjkar3@student.monash.edu.au>
2022-10-17 22:45:29 +1100

    Removed unused data.

--- DataViz2/vega_lite_vis.js
+++ DataViz2/vega_lite_vis.js
@@ -6,7 +6,3 @@ var vg_2 = "global_co2_emissions.vg.json";
 vegaEmbed("#global_co2_emissions", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_3 = "global_temperature_changes.vg.json";
-vegaEmbed("#global_temperature_changes", vg_3).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
