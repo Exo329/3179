a0ddabbfbd08d660309f56b45573f5f0baa7ce0a
sam_karopoulos <sjkar3@student.monash.edu.au>
2022-10-17 17:42:57 +1100

    Removing action buttons didn't work, setting it back.

--- DataViz2/vega_lite_vis.js
+++ DataViz2/vega_lite_vis.js
@@ -1,8 +1,8 @@
 var vg_1 = "global_average_surface_temperature_over_time.vg.json";
-vegaEmbed("#global_average_surface_temperature_over_time", vg_1, {"actions":false}).then(function(result) {
+vegaEmbed("#global_average_surface_temperature_over_time", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var vg_2 = "global_co2_emissions.vg.json";
-vegaEmbed("#global_co2_emissions", vg_2, {"actions":false).then(function(result) {
+vegaEmbed("#global_co2_emissions", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
